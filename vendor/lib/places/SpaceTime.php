9a3493b8205b2f9097e40df1ea4093ed703998b1
Teeheej <56068908+Teeheej@users.noreply.github.com>
2021-05-20 08:39:55 +0700

    Delete vendor directory

--- vendor/lib/places/SpaceTime.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class SpaceTime {
-    public function skipTime($hours) {
-	echo "\n\n..." . $hours . " hours later...\n";
-    }
-}
--- vendor/lib/things/instruments/Bass.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-class Bass extends Guitar {
-    function produceSthBeautiful($who) {
-	echo "\n" . $who . " plays smooth and a bit spicy bass line";
-    }
-}
