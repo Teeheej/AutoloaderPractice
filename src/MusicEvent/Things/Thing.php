bef0a0b9963b01648c63a1bb186004aac169e6d4
Teeheej <56068908+Teeheej@users.noreply.github.com>
2021-05-20 08:40:28 +0700

    Delete src directory

--- src/MusicEvent/Things/Thing.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace MusicEvent\Things;
-
-
-class Thing {}
--- src/autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function autoloader($className) {
-    require_once __DIR__ . '/' . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
-}
-
-spl_autoload_register('autoloader');
